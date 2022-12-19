1de1426a14a82e232c14da17b0eb408c4daa1c56
Blue <lprwasunavailable@gmail.com>
2022-12-19 01:07:09 +0100

    updated image url

--- src/util/ProjectsList/ProjectsList.js
+++ src/util/ProjectsList/ProjectsList.js
@@ -3,7 +3,7 @@ const projects = {
         name: "likealight",
         decoName: "Like A Light",
         shortDesc: "Discord bot made in JS with DJS and AKAIRO",
-        thumbnail: "https://cdn.discordapp.com/attachments/936748287707471933/1004237392464576593/newLAL.png?size=512",
+        thumbnail: "https://github.com/BluePotatoBP/portfolio-website/blob/master/public/lalLogo512.png?raw=true",
         repo: "bluepotatobp/lal-akairo",
         tags: ["lal", "likealight", "light", "akairo", "discord", "discordjs", "like a light"]
     },
