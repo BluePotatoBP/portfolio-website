e97df4d8d4f21671efbe98370b6b1dac9241016c
Blue <lprwasunavailable@gmail.com>
2023-01-23 21:21:44 +0100

    started work on project cards

--- src/components/High-Order/Project/Project.jsx
+++ src/components/High-Order/Project/Project.jsx
@@ -6,24 +6,26 @@ import AboutProject from "../AboutProject/AboutProject.jsx"
 import ProjectDescription from "../ProjectDescription/ProjectDescription";
 
 import { FaGithubSquare } from 'react-icons/fa';
+import { MdImageNotSupported } from "react-icons/md";
 
 const Project = () => {
     const { project } = useParams();
     const projectData = getProject(project);
-    const GITHUB_URL = "https://github.com/"
+
+    const githubUrl = `https://github.com/${projectData.repo}`
 
     return (
         <div className="project-container">
             <div className="project-top-row">
                 <div className="project-top-row-title">
-                    <img src={projectData.thumbnail} alt="thumbnail" draggable={false} className="project-thumbnail" />
+                    {projectData.thumbnail ? <img src={projectData.thumbnail} alt="thumbnail" draggable={false} className="project-thumbnail" /> : <MdImageNotSupported className="project-thumbnail fallback" />}
                     <div className="project-summary">
                         <h2>{projectData.decoName}</h2>
                         <span>{projectData.shortDesc}</span>
                     </div>
                 </div>
                 <div className="project-summary-icons">
-                    <a href={GITHUB_URL + projectData.repo} target="_blank" rel="noreferrer"><FaGithubSquare className="project_summary_icon" /></a>
+                    <a href={githubUrl} target="_blank" rel="noreferrer"><FaGithubSquare className="project_summary_icon" /></a>
                 </div>
             </div>
             <div className="project-description-row">
--- src/components/High-Order/Projects/Projects.jsx
+++ src/components/High-Order/Projects/Projects.jsx
@@ -1,18 +1,56 @@
 import "./Projects.css";
+import { useState } from "react";
+import { getProjects } from "../../../util/ProjectsList/ProjectsList";
+import Card from "../../Low-Order/Card/Card";
+
+import { MdExpandMore } from "react-icons/md";
 
 const Projects = () => {
-    
+    // Getting the projects
+    const projects = Object.values(getProjects());
+
+    // State variable to keep track of the number of displayed projects
+    const [displayedProjects, setDisplayedProjects] = useState(8);
+
+    // Function to return the current list of projects
+    const listProjects = (amount, onlyFeatured = false) =>
+        projects.slice(0, amount).map(p => {
+            if (onlyFeatured && !p.featured) return null;
+            if (p.hidden) return null;
+            return <Card decoration={p} key={p.index} />;
+        });
+
+    // Function to handle the load more button click
+    const handleLoadMoreClick = () => {
+        setDisplayedProjects(displayedProjects + 4);
+    };
+
     return (
-        <div className="projects-container">
-            <h1>Projects</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-                voluptatibus, quod, quia, voluptates quae voluptatem voluptate
-                voluptas quos quibusdam laborum consequatur. Quisquam, quia
-                voluptates. Quaerat, quos. Quisquam, quia. Quo, quas.
-            </p>
+        <div className="projects-container" >
+            <div className="projects-main-display">
+                <div className="block-paragraph right">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum doloribus et adipisci iure, minima recusandae nisi aperiam odio voluptas rem, asperiores libero expedita exercitationem, sint aliquid perferendis. Velit nostrum omnis mollitia nesciunt, neque ullam placeat.</p>
+                </div>
+                <div className="featured-list" >
+                    <div className="title-large featured">Featured</div>
+                    <div className="featured-list-items" >
+                        {listProjects(3, true)}
+                    </div>
+                </div>
+                <div className="block-paragraph left">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus asperiores culpa porro voluptatibus! Aliquid, maxime? Cumque blanditiis nam officia esse.</p>
+                    <p>Lorem ipsum sit amet consectetur elit. Possimus asperiores culpa porro voluptatibus!</p>
+                </div>
+                <div className="projects-list">
+                    <h1 className="title-large">All Projects</h1>
+                    <div className="projects-list-items" >
+                        {listProjects(displayedProjects)}
+                    </div>
+                    {displayedProjects < projects.length && (<button className="load-more-btn" title="Press to load more projects." onClick={handleLoadMoreClick} ><MdExpandMore /></button>)}
+                </div>
+            </div>
         </div>
     );
-}
+};
 
-export default Projects
+export default Projects;
