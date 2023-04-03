b80ae10a2765c3a418b726ac422bd23aa0cf50f7
Blue <lprwasunavailable@gmail.com>
2023-04-03 15:25:51 +0200

    the eternal formatting debate

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import ReactDOM from "react-dom/client";
 import React from "react";
 
-import App from "./components/High-Order/App/App"
+import App from "./components/High-Order/App/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(
-        <App />
-);
+root.render(<App />);
