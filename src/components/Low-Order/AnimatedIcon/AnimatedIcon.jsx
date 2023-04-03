35bdac4bd52c90057a1fc27e0e1ce1850d02763a
Blue <lprwasunavailable@gmail.com>
2023-04-03 17:18:08 +0200

    made the icon reusable

--- src/components/Low-Order/AnimatedIcon/AnimatedIcon.jsx
+++ src/components/Low-Order/AnimatedIcon/AnimatedIcon.jsx
@@ -1,12 +1,22 @@
-import React from "react"
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-import './AnimatedIcon.css';
+import "./AnimatedIcon.css";
 
-const AnimatedIcon = () => {
-    return (
-        <Link to="/" draggable={false} className='animated_logo'>&#9863;</Link>
-    )
-}
+const AnimatedIcon = ({fontSize = "2.4rem", isLink = true}) => {
+	if(isLink) {
+		return (
+			<Link to="/" draggable={false} style={{ fontSize: fontSize, userSelect: "none" }} className="animated_logo">
+				&#9863;
+			</Link>
+		);
+	} else {
+		return (
+			<div style={{ fontSize: fontSize, userSelect: "none" }} className="animated_logo">
+				&#9863;
+			</div>
+		);
+	}
+};
 
-export default AnimatedIcon
+export default AnimatedIcon;
