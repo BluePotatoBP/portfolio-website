a932c0e8f59e4845b05ccf8217398f5daeae1116
Blue <lprwasunavailable@gmail.com>
2023-04-04 16:23:05 +0200

    formatting

--- src/components/High-Order/Footer/Footer.jsx
+++ src/components/High-Order/Footer/Footer.jsx
@@ -6,17 +6,17 @@ import { BsTwitter, BsDiscord, BsGithub } from "react-icons/bs"
 
 const Footer = () => {
 
-    return (
-        <footer className="footer-container">
-            <div className="footer-content">
-                <div className="footer-socials">
-                    <a href="https://twitter.com/BluePotatoBP" draggable={false} target="_blank" rel='noreferrer' aria-label="twitter" className="link"><BsTwitter className='icon' /></a>
-                    <a href="https://discord.gg/v8zkSc9" draggable={false} target="_blank" rel='noreferrer' aria-label="discord" className="link"><BsDiscord className='icon' /></a>
-                    <a href="https://github.com/BluePotatoBP" draggable={false} target="_blank" rel='noreferrer' aria-label="github" className="link"><BsGithub className='icon' /></a>
-                </div>
-            </div>
-        </footer>
-    )
+	return (
+		<footer className="footer-container">
+			<div className="footer-content">
+				<div className="footer-socials">
+					<a href="https://twitter.com/BluePotatoBP" draggable={false} target="_blank" rel='noreferrer' aria-label="twitter" className="link"><BsTwitter className='icon' /></a>
+					<a href="https://discord.gg/v8zkSc9" draggable={false} target="_blank" rel='noreferrer' aria-label="discord" className="link"><BsDiscord className='icon' /></a>
+					<a href="https://github.com/BluePotatoBP" draggable={false} target="_blank" rel='noreferrer' aria-label="github" className="link"><BsGithub className='icon' /></a>
+				</div>
+			</div>
+		</footer>
+	)
 }
 
 export default Footer
