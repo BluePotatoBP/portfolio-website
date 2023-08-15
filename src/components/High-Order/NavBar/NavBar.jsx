c81cc8c924e8150b88210cce6510bd75ae967385
Blue <lprwasunavailable@gmail.com>
2023-08-16 00:35:24 +0200

    made sticky, changes blur amount on scroll

--- src/components/High-Order/NavBar/NavBar.jsx
+++ src/components/High-Order/NavBar/NavBar.jsx
@@ -6,11 +6,27 @@ import { FiExternalLink } from 'react-icons/fi';
 
 import SearchBar from '../SearchBar/SearchBar';
 import AnimatedIcon from '../../Low-Order/AnimatedIcon/AnimatedIcon';
+import { useEffect, useState } from 'react';
 
 const NavBar = () => {
+	// Apply darker background on first scroll
+	const [hasScrolled, setScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => {
+			if(window.scrollY > 0) setScrolled(true)
+			else setScrolled(false)
+		}
+		// Create event listener
+		window.addEventListener('scroll', handleScroll);
+		// Remove event listener when the event is over
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		}
+	}, [])
 
 	return (
-		<nav className="navbar-container">
+		<nav className="navbar-container" scrolled={hasScrolled.toString()}>
 			<AnimatedIcon />
 			<div className="navbar-helper-container">
 				<div className='navbar-links'>
