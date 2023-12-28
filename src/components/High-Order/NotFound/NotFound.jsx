0b1ba27fc4ad7e71c7f3bf9c58369251e3fb643a
Blue <lprwasunavailable@gmail.com>
2023-12-28 10:22:41 +0100

    finalized not found page

--- src/components/High-Order/NotFound/NotFound.jsx
+++ src/components/High-Order/NotFound/NotFound.jsx
@@ -1,18 +1,22 @@
 import "./NotFound.css";
 
 const NotFound = () => {
-    
-    return (
-        <div className="not-found-container">
-            <h1>GeorgeNotFound</h1>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-                voluptatibus, quod, quia, voluptates quae voluptatem voluptate
-                voluptas quos quibusdam laborum consequatur. Quisquam, quia
-                voluptates. Quaerat, quos. Quisquam, quia. Quo, quas.
-            </p>
-        </div>
-    );
+
+	const titles = ["Not Found.", "404 Not Found.", "Couldn't find that page.", "Sorry, that page doesn't exist."]
+	const descriptions = ["Maybe the page got moved.", "Couldn't find that page.", "This page doesnt exist."]
+
+	const randomTitle = Math.floor(Math.random() * titles.length)
+	const randomDescription = Math.floor(Math.random() * descriptions.length)
+
+
+	return (
+		<div className="not-found-container">
+			<div className="not-found-content">
+				<div className="not-found-title">{titles[randomTitle]}</div>
+				<div className="not-found-description">{descriptions[randomDescription]}</div>
+			</div>
+		</div>
+	);
 }
 
 export default NotFound
