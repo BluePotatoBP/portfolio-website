e5f47e999f6424e2b6c8ede5562d782379775e9c
Blue <lprwasunavailable@gmail.com>
2023-12-28 03:18:24 +0100

    refactor, allowed linking

--- src/components/Low-Order/HighlightedText/HighlightedText.jsx
+++ src/components/Low-Order/HighlightedText/HighlightedText.jsx
@@ -1,30 +1,45 @@
 import "./HighlightedText.css";
+import { Link } from "react-router-dom"
 
 const HighlightedText = (
 	{
 		text,
 		image,
+		link,
 		backgroundColor = "#3e455b",
 		textColor = "#fff",
 		textShadowColor = null,
 		textShadowSize = "10px",
-		padding = "0 0.2rem 0 0.2rem",
+		padding = "0 0.5rem 0 0.5rem",
 		margin = "0 0.2rem 0 0.2rem",
-		borderRadius = "0.2rem"
-	}) => {
-
+		borderRadius = "0.5rem"
+	}
+) => {
 	const textShadowStyle = textShadowColor ? `1px 1px ${textShadowSize} ${textShadowColor}` : null
+	const styling = { backgroundColor: backgroundColor, color: textColor, padding: padding, margin: margin, borderRadius: borderRadius }
 
-	return (
-		<div className="highlighted-text-container" style={{ backgroundColor: backgroundColor, color: textColor, padding: padding, margin: margin, borderRadius: borderRadius }}>
-			{image ? (
-				<div className="highlighted-img">
-					<img src={image} alt="highlighted" draggable={false} />
-				</div>
-			) : null}
-			<div className="highlighted-text" style={{ textShadow: textShadowStyle }}>{text}</div>
+	const imageElement = (
+		<div className="highlighted-img">
+			<img src={image} alt="highlighted" draggable={false} />
 		</div>
-	);
+	)
+
+	// If theres a link use react router
+	if (link) {
+		return (
+			<Link to={link ? link : null} className="highlighted-text-container" style={styling}>
+				{image ? imageElement : null}
+				<div className="highlighted-text" style={{ textShadow: textShadowStyle }}>{text}</div>
+			</Link>
+		);
+	} else {
+		return (
+			<div to={link ? link : null} className="highlighted-text-container" style={styling}>
+				{image ? imageElement : null}
+				<div className="highlighted-text" style={{ textShadow: textShadowStyle }}>{text}</div>
+			</div>
+		);
+	}
 };
 
 export default HighlightedText;
